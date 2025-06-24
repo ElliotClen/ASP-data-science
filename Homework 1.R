0bbe2bf66af504d946c0d1ec67da89c51a44d0cf
ElliotClen <141185126+ElliotClen@users.noreply.github.com>
2025-06-24 13:56:52 -0400

    Add files via upload

---
+++ Homework 1.R
@@ -0,0 +1,34 @@
+# Homework 1 / June 24, 2025
+
+# Create a variable called a that is any negative integer.
+
+# Create a variable called b that is a positive decimal between 0 and 1.
+
+# Create a variable abs_a that stores the absolute value of a.
+
+# Create a variable log_b that stores the log base 10 of b.
+
+# Create a variable a_squared that stores a raised to the power of 2.
+
+# Create a variable quotient that stores the integer result of dividing 100 by 9.
+
+# Create a vector called math_vec that contains all six variables from above.
+
+#Use functions only to generate the following vectors (Do not hard-code the values):
+  # A vector containing every 5th number from 5 to 100
+
+  # A vector that starts at 50, goes down by 5, and stops at 5
+
+  # A vector that repeats the numbers (2, 4, 6) 15 times in a row
+
+  # A vector that repeats: sixteen 2s in a row then ten 4s in a row then ten 6s in a row
+
+# Create a vector called rand_nums containing 30 random integers from 1 to 100 (allow repeats).
+
+# Find the mean, median, and max of rand_nums
+
+# Create a vector with 5 grocery items
+
+# Create a vector with 5 prices
+
+# Use the paste command to print out the item and price in the format "The price of ITEM is PRICE"
---
+++ Practice1.R
@@ -0,0 +1,93 @@
+# R Practice 1 / June 24, 2025
+
+## Part 1 - In Class Demo
+
+setwd()
+
+# Add
+
+# Subtract
+
+# Multiply
+
+# Divide
+
+# Modulus
+
+# Absolute Value
+
+# Square Root
+
+# e^
+
+# ln
+
+# log base 10
+
+# Vectors
+
+# sum
+
+# length
+
+# mean
+
+# median
+
+# min
+
+# max
+
+# rep
+
+# seq
+
+# sample
+
+# paste
+
+## Part 2 - Practice
+
+# Math Functions: + - * / %% abs sqrt exp log log10
+# Vector Functions: sum, length, mean, median, min, max, rep, seq, sample
+
+# Create a variable called x that is an integer
+
+# Create a variable called y that is a number with a decimal
+
+# Create a variable called xroot that is the square root of x
+
+# Create a variable called ln_y that is ln(y)
+
+# Create a variable called rem that is the remainder of dividing 72 by 7
+
+# Create a vector called vec that contains all 5 of the previously created variables
+
+# Print out the following vectors using functions, do not hard code the values
+# (1, 2, 3, . . . , 19, 20)
+
+# (20, 19, . . . , 2, 1)
+
+# (1, 2, 3, . . . , 19, 20, 19, 18, . . . , 2, 1)
+
+# (4, 6, 3) and assign it to the name tmp
+
+# (4, 6, 3, 4, 6, 3, . . . , 4, 6, 3) where there are 10 occurrences of 4.
+
+# (4, 6, 3, 4, 6, 3, . . . , 4, 6, 3, 4) where there are 11 occurrences of 4, 10 occurrences of 6 and 10 occurrences of 3.
+
+# (4, 4, . . . , 4, 6, 6, . . . , 6, 3, 3, . . . , 3) where there are 10 occurrences of 4, 20 occurrences of 6 and 30 occurrences of 3.
+
+# Create a vector called random that has 50 random values from 1 through 1000
+
+# Find the average of random without using the mean() function
+
+# Find the maximum value in random
+
+# Find the minimum value in random
+
+# Create a vector of 5 student names
+
+# Create a vector of 5 ages
+
+# Use the paste command to print out the name and age of each student in the format, "NAME is AGE years old"
