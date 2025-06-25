cc25c7f289ac3a3ecc9a2e3d58c3e13030e5c4f4
ElliotClen <141185126+ElliotClen@users.noreply.github.com>
2025-06-25 06:38:57 -0400

    Add files via upload

---
+++ day2/Practice2.R
@@ -0,0 +1,33 @@
+# R Practice 2 / June 25, 2025
+
+## Part 1 - In Class Demo
+
+# load the pre-installed state dataset
+data(state)
+# convert it to a useable form
+state = as.data.frame(state.x77)
+# add the region variable
+state$region = state.region
+# add the division variable
+state$division = state.division
+
+# take a quick look at the dataset
+
+# make a histogram of population
+
+# make a boxplot of population
+
+# make a table of region
+
+# make a barchart of region
+
+# make a piechart of region
+
+## Part 2 - Practice
+
+# make at least two categorical graphs and four numeric graphs
+# add titles, colors, and labels!
+
+# can you figure out how to make a boxplot vertical?
+
+# can you figure out how to make each pieve a different color in a histogram, bar, or pie?
